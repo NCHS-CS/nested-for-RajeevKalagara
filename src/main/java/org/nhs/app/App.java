3ca887bd9ede2b38829930b36629d5407bdf7454
RajeevKalagara <2008377@apps.nsd.org>
2024-10-07 04:00:15 +0000

    Done

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,72 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+public class App
+{
+    public static void main(String args[])
+    {
+        /*for(int i = 1; i <= 5; i++)
+        {
+            for(int count = 1; count <=4; count++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }*/
 
-package org.nhs.app;
+       practice1();
+       System.out.println();
+       practice2();
+       System.out.println();
+       practice3();
 
-// Your code goes here
+    }
+
+    public static void practice1()
+    {
+        for(int i = 1; i <= 5; i++)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void practice2()
+    {
+        for(int i = 1; i <= 5; i++)
+        {
+            for (int j = 5; j > i; j--)
+            {
+                System.out.print(".");
+            }
+
+            for(int k = 1; k <=i; k++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void practice3()
+    {
+        for(int i = 1; i <= 5; i++)
+        {
+            for (int j = 5; j > i; j--)
+            {
+                System.out.print(".");
+            }
+
+            System.out.print(i);
+
+            for(int k = 1; k <i; k++)
+            {
+                System.out.print(".");
+            }
+
+            
+            System.out.println();
+        }
+    }
+}
